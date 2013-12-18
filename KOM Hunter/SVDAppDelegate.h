43946420bc3cca4c5dc47a1f600995535a5491d3
Adam Chamberlin <adam@shibbyonline.co.uk>
2013-12-18 09:47:51 +0000

    Initial Commit

---
+++ KOM Hunter/SVDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVDAppDelegate.h
+//  KOM Hunter
+//
+//  Created by Adam Chamberlin on 18/12/2013.
+//  Copyright (c) 2013 Adam Chamberlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
